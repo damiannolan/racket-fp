145e3c3f0ceedd0fe3497040d75a26d08405fee1
Damian Nolan <damiannolan@gmail.com>
2018-02-05 16:06:24 +0000

    updated collatz-list and added additional comments

--- collatz-list.rkt
+++ collatz-list.rkt
@@ -1,6 +1,11 @@
 #lang racket
 
-; The Collatz Conjecture
+; The Collatz Conjecture - using recursive lists
+
+; The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows: start with any
+; positive integer n. Then each term is obtained from the previous term as follows: if the previous term is even, the
+; next term is one half the previous term. Otherwise, the next term is 3 times the previous term plus 1. The conjecture
+; is that no matter what value of n, the sequence will always reach 1.
 
 (define (collatz-list n)
   (collatz-conj n (list n)))
@@ -12,4 +17,8 @@
 	  (collatz-conj (/ n 2) (append l (list (/ n 2))))
 	  (collatz-conj (+ 1 (* n 3)) (append l (list (+ 1 (* n 3))))))))
 
-(collatz-list 5)
+(collatz-list 5) ; '(5 16 8 4 2 1)
+
+(collatz-list 9) ; '(9 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1)
+
+(collatz-list 2) ; '(2 1)
