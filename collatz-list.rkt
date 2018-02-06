ccb55ac921fad7d26ac4db30d48002af549ed4eb
Damian Nolan <damiannolan@gmail.com>
2018-02-06 01:38:32 +0000

    added append-list from scratch using null?, car, cons, cdr

--- collatz-list.rkt
+++ collatz-list.rkt
@@ -7,6 +7,23 @@
 ; next term is one half the previous term. Otherwise, the next term is 3 times the previous term plus 1. The conjecture
 ; is that no matter what value of n, the sequence will always reach 1.
 
+; It is possible to construct an append function which is written from scratch using only null?, cons, car and cdr
+; cons - takes two args. If the 2nd arg is a list 
+; -> makes the 1st arg the first of the new list
+; -> makes the 2nd arg the rest of the new list
+; car list1 -> 1 cdr 2 3 -> 1 2 cdr 3 -> 1 2 3 cdr null -> 1 2 3 4 5 6
+
+; car list1 is 1 cdr is 2 3
+; car list1 is 2 cdr is 3
+; car list1 is 3 cdr is null
+; cons 1 2 3 with 4 5 6 -> '(1 2 3 4 5 6)
+(define (append-list list1 list2)
+  (if (null? list1)
+	list2
+	(cons (car list1) (append-list (cdr list1) list2))))
+
+(append-list '(1 2 3) '(4 5 6)) ; '(1 2 3 4 5 6)
+
 (define (collatz-list n)
   (collatz-conj n (list n)))
 
@@ -14,8 +31,8 @@
   (if (= n 1)
 	l
 	(if (= (modulo n 2) 0)
-	  (collatz-conj (/ n 2) (append l (list (/ n 2))))
-	  (collatz-conj (+ 1 (* n 3)) (append l (list (+ 1 (* n 3))))))))
+	  (collatz-conj (/ n 2) (append-list l (list (/ n 2))))
+	  (collatz-conj (+ 1 (* n 3)) (append-list l (list (+ 1 (* n 3))))))))
 
 (collatz-list 5) ; '(5 16 8 4 2 1)
 
