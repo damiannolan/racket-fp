c8d9f603499a9ab694979e5c0ef7191018f86609
Damian Nolan <damiannolan@gmail.com>
2018-03-13 01:52:26 +0000

    refactored some namings and organised functions, needs more clean up

--- maj.rkt
+++ maj.rkt
@@ -7,43 +7,37 @@
 ; (maj (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
 ; '(0 0 0 1 0 1 1 1)
 
-; This produces '(0 1 1 2 1 2 2 3)
-; maybe if > 1 then cons 1 otherwise cons 0
-(define (temp x y z)
-  (temp2 (map (lambda (x y z)(+ x y z)) x y z)))
+(map (lambda (x y z) (+ x y z))
+  '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
 
-(define (temp2 lst)
+; The map function above produces '(0 1 1 2 1 2 2 3)
+; Therefore, if an element of the output list is greater than 1, it has occurred more than once
+; Create an auxiliary function to recursively loop through the list and produce a new list
+; containing only 0's and 1's. 
+; maybe if > 1 then cons 1 otherwise cons 0
+(define (maj x y z)
+  (maj-aux (map (lambda (x y z) (+ x y z)) x y z)))
+
+(define (maj-aux lst)
   (if (null? lst)
 	'()
 	(if (> (car lst) 1)
-	  (cons 1 (temp2 (cdr lst)))
-	  (cons 0 (temp2 (cdr lst))))))
+	  (cons 1 (maj-aux (cdr lst)))
+	  (cons 0 (maj-aux (cdr lst))))))
 
-(temp '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
-;(map (lambda (x y z)
-;	   (+ x y z))
-;	   '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
+(maj '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
 
 (define (majority x y z)
-  (converge (maj-aux x y) (maj-aux x z) (maj-aux y z)))
+  (converge (majority-aux x y) (majority-aux x z) (majority-aux y z)))
 
-(define (maj-aux x y)
+(define (majority-aux x y)
   (if (null? x)
 	'()
     (if (= 1 (car x)) 
 	  (if (= 1 (car y))
-	    (cons 1 (maj-aux (cdr x) (cdr y)))
-	    (cons 0 (maj-aux (cdr x) (cdr y))))
-      (cons 0 (maj-aux (cdr x) (cdr y))))))
-
-(define (maj-or x y z)
-  (if (null? x)
-	'()
-	(if (and (or (= 1 (car x)) (= 1 (car y)) (= 1 (car z))))
-	  (cons 1 (maj-or (cdr x) (cdr y) (cdr z)))
-	  (cons 0 (maj-or (cdr x) (cdr y) (cdr z))))))
-
-(maj-or '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
+	    (cons 1 (majority-aux (cdr x) (cdr y)))
+	    (cons 0 (majority-aux (cdr x) (cdr y))))
+      (cons 0 (majority-aux (cdr x) (cdr y))))))
 
 (define (converge x y z)
   (if (null? x)
@@ -52,25 +46,26 @@
 	  (cons 1 (converge (cdr x) (cdr y) (cdr z)))
 	  (cons 0 (converge (cdr x) (cdr y) (cdr z))))))
 
-(maj-aux '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1)) ; x y
-(maj-aux '(0 0 0 0 1 1 1 1) '(0 1 0 1 0 1 0 1)) ; x z
-(maj-aux '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1)) ; y z
+(majority-aux '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1)) ; x y
+(majority-aux '(0 0 0 0 1 1 1 1) '(0 1 0 1 0 1 0 1)) ; x z
+(majority-aux '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1)) ; y z
 (majority '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
 
-(define (maj x y z)
+; The naive way
+(define (maj-naive x y z)
   (if (null? x)
 	'()
 	(if (= 1 (car x))
 	  (if (= 1 (car y)) 
-		(cons 1 (maj (cdr x) (cdr y) (cdr z))) ; x is 1, y is 1
+		(cons 1 (maj-naive (cdr x) (cdr y) (cdr z))) ; x is 1, y is 1
 		(if (= 1 (car z))
-		  (cons 1 (maj (cdr x) (cdr y) (cdr z))) ; x is 1, y is 0, z is 1
-		  (cons 0 (maj (cdr x) (cdr y) (cdr z))))) ; x is 1, y is 0, z is 0
+		  (cons 1 (maj-naive (cdr x) (cdr y) (cdr z))) ; x is 1, y is 0, z is 1
+		  (cons 0 (maj-naive (cdr x) (cdr y) (cdr z))))) ; x is 1, y is 0, z is 0
 	  (if (= 1 (car y))
 	    (if (= 1 (car z))
-		  (cons 1 (maj (cdr x) (cdr y) (cdr z))) ; x is 0, y is 1, z is 1
-		  (cons 0 (maj (cdr x) (cdr y) (cdr z)))) ; x is 0, y is 1, z is 0
-	    (cons 0 (maj (cdr x) (cdr y) (cdr z))))))) ; x is 0, y is 0, z is 0
+		  (cons 1 (maj-naive (cdr x) (cdr y) (cdr z))) ; x is 0, y is 1, z is 1
+		  (cons 0 (maj-naive (cdr x) (cdr y) (cdr z)))) ; x is 0, y is 1, z is 0
+	    (cons 0 (maj-naive (cdr x) (cdr y) (cdr z))))))) ; x is 0, y is 0, z is 0
 
-(maj '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
+(maj-naive '(0 0 0 0 1 1 1 1) '(0 0 1 1 0 0 1 1) '(0 1 0 1 0 1 0 1))
 
